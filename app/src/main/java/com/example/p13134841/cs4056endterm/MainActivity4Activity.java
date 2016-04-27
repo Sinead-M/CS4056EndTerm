df84014606e3aeb769cbd020959eb48cb3d9f196
LouBergs <louiseberg94@gmail.com>
2016-04-27 23:15:43 +0100

    Added menu item arrays for menu screens.

--- app/src/main/java/com/example/p13134841/cs4056endterm/MainActivity4Activity.java
+++ app/src/main/java/com/example/p13134841/cs4056endterm/MainActivity4Activity.java
@@ -1,16 +1,20 @@
 package com.example.p13134841.cs4056endterm;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity4Activity extends AppCompatActivity {
 
     TextView restTitle;
     String restaurantName = "";
+
+    LinearLayout menuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,31 @@ public class MainActivity4Activity extends AppCompatActivity {
         restTitle = (TextView) findViewById(R.id.RestTitle);
         restTitle.setText(restaurantName);
 
+        menuView = (LinearLayout) findViewById(R.id.menuView);
+        menuView.setBackgroundColor(Color.WHITE);
+        String[] menuItems;
+
+        //CHOOSE MENU ARRAY BASED ON RESTAURANT NAME.
+
+        if(restaurantName.equals("STABLES")) {
+            menuItems = getApplicationContext().getResources().getStringArray(R.array.stables_menu);
+        } else if(restaurantName.equals("PADDOCKS")) {
+            menuItems = getApplicationContext().getResources().getStringArray(R.array.paddocks_menu);
+        } else if(restaurantName.equals("SCHOLARS")) {
+            menuItems = getApplicationContext().getResources().getStringArray(R.array.scholars_menu);
+        } else {
+            menuItems = getApplicationContext().getResources().getStringArray(R.array.eden_menu);
+        }
+
+        //ADD MENU ITEM TO VIEW FOR EACH STRING.
+
+        for(String item : menuItems) {
+            TextView tv = new TextView(getApplicationContext());
+            tv.setText(item);
+            tv.setTextColor(Color.BLACK);
+            menuView.addView(tv);
+        }
+
         Button dealsbutton = (Button)findViewById(R.id.dealstab);
         dealsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +63,8 @@ public class MainActivity4Activity extends AppCompatActivity {
                 myBundle.putString(MainActivity2Activity.KEY, restaurantName);
                 intent.putExtras(myBundle);//PASS BUNDLE TO ACTIVITY 4
                 startActivity(intent);
-                finish();       }
+                finish();
+            }
         });
 
         Button locationbutton = (Button)findViewById(R.id.locationtab);
