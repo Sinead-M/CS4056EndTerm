cbce9c0581b2b268606ec04c4a7bd404de6e5e66
Sinead Maher <sineadm3@gmail.com>
2016-04-28 16:55:27 +0100

    Added grid view to today's deal

--- app/src/main/java/com/example/p13134841/cs4056endterm/MainActivity3Activity.java
+++ app/src/main/java/com/example/p13134841/cs4056endterm/MainActivity3Activity.java
@@ -4,9 +4,14 @@ package com.example.p13134841.cs4056endterm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,8 +20,8 @@ import java.util.ArrayList;
 
 public class MainActivity3Activity extends AppCompatActivity {
 
-    ArrayList<String> names;
-    ArrayAdapter<String> namesAdapter;
+    ArrayList<Thumbnail> thumbnails;
+    MainActivity3Activity.GridAdapter gridAdapter;
     TextView restTitle;
     String restaurantName = "";
 
@@ -65,38 +70,72 @@ public class MainActivity3Activity extends AppCompatActivity {
     }
 
     private void setup() {
-        names = new ArrayList<>();
+        thumbnails = new ArrayList<>();
 
         if (restaurantName.equals("The Stables Club")) {
             //show stables deals...etc.
-            names.add("DEAL 1");
-            names.add("DEAL 2");
-            names.add("DEAL 3");
-            names.add("DEAL 4");
+            thumbnails.add(new Thumbnail(R.drawable.goatscheesetart, "Goats cheese and caramelised onion tart"));
+            thumbnails.add(new Thumbnail(R.drawable.tomatosoup, "Tomato & basil soup"));
+            thumbnails.add(new Thumbnail(R.drawable.chickenwrap, "Spicy Chicken fajita wrap"));
+            thumbnails.add(new Thumbnail(R.drawable.haddock, "Smoked haddock with onion sauce"));
 
         } else if (restaurantName.equals("The Scholars Club")) {
             //show scholars deals...etc.
-            names.add("DEAL 5");
-            names.add("DEAL 6");
-            names.add("DEAL 7");
-            names.add("DEAL 8");
+            thumbnails.add(new Thumbnail(R.drawable.peasoup, "Cream of Pea & Carrot Soup"));
+            thumbnails.add(new Thumbnail(R.drawable.ribs, "BBQ pork spare ribs"));
+            thumbnails.add(new Thumbnail(R.drawable.lasagne, "Vegetarian lasagne"));
+            thumbnails.add(new Thumbnail(R.drawable.pork, "Roast Loin of Pork with Apple Sauce"));
 
         } else if (restaurantName.equals("The Paddocks")) {
-            names.add("DEAL 9");
-            names.add("DEAL 10");
-            names.add("DEAL 11");
-            names.add("DEAL 12");
+            thumbnails.add(new Thumbnail(R.drawable.sandwichsoup, "Toasted  sandwich & vegetable soup"));
+            thumbnails.add(new Thumbnail(R.drawable.volauvent, "Chicken & mushroom vol au vent & chips "));
+            thumbnails.add(new Thumbnail(R.drawable.codchips, "Baked fillet of cod & chips"));
+            thumbnails.add(new Thumbnail(R.drawable.blt, "BLT with chips/salad"));
 
         } else {
-            names.add("DEAL 13");
-            names.add("DEAL 14");
-            names.add("DEAL 15");
-            names.add("DEAL 16");
+            thumbnails.add(new Thumbnail(R.drawable.seafoodpie, "Atlantic seafood pie"));
+            thumbnails.add(new Thumbnail(R.drawable.burger, "Irish beef burger"));
+            thumbnails.add(new Thumbnail(R.drawable.springrolls, "Vegetarian spring rolls"));
+            thumbnails.add(new Thumbnail(R.drawable.amatricana, "Wholewheat pasta Amatricana"));
         }
 
-        namesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        gridAdapter = new MainActivity3Activity.GridAdapter();
 
-        ListView namesListView = (ListView) findViewById(R.id.listView);
-        namesListView.setAdapter(namesAdapter);
+        GridView imagesGridView = (GridView) findViewById(R.id.images_grid_view);
+        imagesGridView.setAdapter(gridAdapter);
+    }
+    public class GridAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return thumbnails.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return thumbnails.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewGroup itemRootView;
+            if (convertView == null) {
+                itemRootView = (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
+            }
+            else {
+                itemRootView = (ViewGroup) convertView;
+            }
+            Thumbnail thumbnail = thumbnails.get(position);
+            TextView gridTextView = (TextView) itemRootView.findViewById(R.id.grid_text_view);
+            gridTextView.setText(thumbnail.getName());
+            ImageView gridImageView = (ImageView) itemRootView.findViewById(R.id.grid_image_view);
+            gridImageView.setImageResource(thumbnail.getImageResId());
+            return itemRootView;
+        }
     }
 }
